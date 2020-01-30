3f7209db942799b7c5a76b531995bd0198a521a9
duhowise <duhwise@gmail.com>
2020-01-30 16:03:10 +0000

    REading and Writing File Contents

---
+++ WordUnscrambler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = { "This is the First Line", "This is the second Line", "Thus is the third line" };
+            File.WriteAllLines("MyFirstFile.txt", lines);
+
+
+            string[] fileContents = File.ReadAllLines("MyFirstFile.txt");
+            foreach (var fileContent in fileContents)
+            {
+                Console.WriteLine(fileContent);
+            }
+        }
+
+        static void ChangeNumber(ref int value)
+        {
+            
+        }
+    }
+}
