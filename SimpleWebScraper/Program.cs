819432216d0bfa52790c049528e66435990ec5b9
duhowise <duhwise@gmail.com>
2020-02-05 12:47:01 +0000

    simple regex matcher

---
+++ SimpleWebScraper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            MatchCollection matches = Regex.Matches("This is my catblah blah...... This is my rat. blah lah lad", "This is my [a-z]at");
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
