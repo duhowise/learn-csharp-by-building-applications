1b03f3b354344bd5a203e94129d1293d3bd59907
duhowise <duhwise@gmail.com>
2020-02-05 13:41:56 +0000

    Added Implementation for scrapper

---
+++ SimpleWebScraper/Workers/Scrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using SimpleWebScraper.Data;
+
+namespace SimpleWebScraper.Workers
+{
+    public class Scrapper
+    {
+        public List<string> Scrape(ScrapeCriteria scrapeCriteria)
+        {
+            var scrappedElements = new List<string>();
+            MatchCollection matches =
+                Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.RegexOption);
+            foreach (Match match in matches)
+            {
+                if (!scrapeCriteria.ScrapeCriteriaParts.Any())
+                {
+                    scrappedElements.Add(match.Groups[0].Value);
+                }
+                else
+                {
+                    foreach (var part in scrapeCriteria.ScrapeCriteriaParts)
+                    {
+                        Match matchedPart = Regex.Match(match.Groups[0].Value, part.Regex, part.RegexOption);
+                        if (matchedPart.Success)
+                        {
+                            scrappedElements.Add(matchedPart.Groups[1].Value);
+                        }
+                    }
+                }
+            }
+
+            return scrappedElements;
+        }
+    }
+}
