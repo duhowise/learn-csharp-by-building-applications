77abb0a4038c08263f899ed19831f61f00d4c9d6
Wise Duho <duhowise@gmail.com>
2020-03-05 16:47:57 +0000

    Binary Literals

---
+++ CsharpSevenSamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CsharpSevenSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte binaryNumber = 0b111;
+            Console.Write(Convert.ToInt16(binaryNumber));
+
+        }
+    }
+}
